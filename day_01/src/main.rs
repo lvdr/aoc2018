c0c34837ec71161accba8d9a399c0fbf8117f6f8
Lavender <lvndspica@gmail.com>
2018-12-03 13:35:28 +0200

    Day 1 cleanup

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,36 +2,34 @@ use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashSet;
+use std::vec::Vec;
 
 fn main()  -> io::Result<()> {
 
-    let mut freq = 0;
+	let mut changes = Vec::new();
+
+    let f = File::open("input.txt")?;
+    let f = BufReader::new(f);
+    for line in f.lines() {
+    	changes.push(line.unwrap().trim().parse::<i32>().unwrap());
+    }
+    println!("Final frequency: {}", changes.to_vec().iter().sum::<i32>());
+
     let mut second_time = 0;
     let mut freqs = HashSet::new();
     freqs.insert(0);
-    let mut got_final_freq = 0;
+    let mut freq = 0;
     while second_time == 0 {
-	    let f = File::open("input.txt")?;
-	    let f = BufReader::new(f);
-	    for line in f.lines() {
-	    	let unwrapped = line.unwrap();
-	    	let change: i32 = unwrapped.trim()[1..].parse().unwrap();
-	    	if unwrapped[0..1].to_string() == "+" {
-	    		freq += change;
-	    	} else {
-	    		freq -= change;
-	    	}
-	    	if second_time == 0 && freqs.contains(&freq) {
+    	for change in &changes {
+    		freq += change;
+	    	if freqs.contains(&freq) {
 	    		println!("Second time seeing {}", freq);
 	    		second_time = 1;
+	    		break;
 	    	} else {
 	    		freqs.insert(freq);
 	    	}
 	    }
-	    if got_final_freq == 0 {
-			println!("Final frequency: {}", freq);
-			got_final_freq = 1;
-		}
 	}
 
     Ok(())
