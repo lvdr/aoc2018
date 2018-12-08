b7c55f1c8d8c6b4c224642cf8da5f4e78c34f181
Lavender <lvndspica@gmail.com>
2018-12-08 13:53:05 +0200

    Reorganization + tests for first two days

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,36 +1,75 @@
-use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashSet;
 use std::vec::Vec;
 
-fn main()  -> io::Result<()> {
 
-	let mut changes = Vec::new();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let f = File::open("input.txt")?;
-    let f = BufReader::new(f);
-    for line in f.lines() {
-    	changes.push(line.unwrap().trim().parse::<i32>().unwrap());
+    #[test]
+    fn test_first_half() {
+        test_final_freq("+1\n-2\n+3\n+1\n", 3);
+        test_final_freq("+1\n+1\n+1\n", 3);
+        test_final_freq("+1\n+1\n-2", 0);
+        test_final_freq("-1\n-2\n-3", -6);
     }
-    println!("Final frequency: {}", changes.to_vec().iter().sum::<i32>());
 
-    let mut second_time = 0;
+    fn test_final_freq(input: &str, result: i32) {
+        let input = String::from(input);
+        let changes = parse_input(&input);
+        assert_eq!(final_frequency(&changes), result);
+    }
+
+    #[test]
+    fn test_second_half() {
+        // Second half
+        test_repeat_freq("+1\n-1\n", 0);
+        test_repeat_freq("+3\n+3\n+4\n-2\n-4\n", 10);
+        test_repeat_freq("-6\n+3\n+8\n+5\n-6", 5);
+        test_repeat_freq("+7\n+7\n-2\n-7\n-4", 14);
+    }
+
+    fn test_repeat_freq(input: &str, result: i32) {
+        let input = String::from(input);
+        let changes = parse_input(&input);
+        assert_eq!(repeat_frequency(&changes), result);
+    }
+}
+
+fn parse_input(input : &String) -> Vec<i32> {
+    input.trim().lines()
+         .map(|x| x.parse::<i32>().unwrap())
+         .collect()
+}
+
+fn final_frequency(changes : &Vec<i32>) -> i32 {
+    changes.iter().sum::<i32>()
+}
+
+fn repeat_frequency(changes: &Vec<i32>) -> i32 {
     let mut freqs = HashSet::new();
-    freqs.insert(0);
     let mut freq = 0;
-    while second_time == 0 {
-    	for change in &changes {
-    		freq += change;
-	    	if freqs.contains(&freq) {
-	    		println!("Second time seeing {}", freq);
-	    		second_time = 1;
-	    		break;
-	    	} else {
-	    		freqs.insert(freq);
-	    	}
-	    }
-	}
+    for change in changes.iter().cycle() {
+        if freqs.contains(&freq) {
+            break;
+        }
+        freqs.insert(freq);
+        freq += change;
+    }
+    freq
+}
 
-    Ok(())
+fn main() {
+    let mut input = String::new();
+    let mut f = File::open("input").expect("Failed to open input.");
+    f.read_to_string(&mut input).expect("Failed to read input.");
+
+	let changes = parse_input(&input);
+    let final_frequency = final_frequency(&changes);
+    println!("Final frequency: {}", final_frequency);
+
+    let repeat_frequency = repeat_frequency(&changes);
+    println!("First repeated frequency: {}", repeat_frequency);
 }
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,10 +1,40 @@
-use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-fn hash(ids: &[String]) -> i32 {
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_first_half() {
+        let input = String::from("abcdef\n\
+                                  bababc\n\
+                                  abbcde\n\
+                                  abcccd\n\
+                                  aabcdd\n\
+                                  abcdee\n\
+                                  ababab");
+        let ids = parse_input(&input);
+        assert_eq!(hash(&ids), 12);
+    }
+
+    #[test]
+    fn test_second_half() {
+        let input = String::from("abcde\n\
+                                  fghij\n\
+                                  klmno\n\
+                                  pqrst\n\
+                                  fguij\n\
+                                  axcye\n\
+                                  wvxyz");
+        let ids = parse_input(&input);
+        assert_eq!(seek_dupes(&ids), String::from("fgij"));
+    }
+}
+
+fn hash(ids: &Vec<String>) -> i32 {
     let mut twos = 0;
     let mut threes = 0;
     for id in ids.into_iter() {
@@ -23,30 +53,36 @@ fn hash(ids: &[String]) -> i32 {
     twos*threes
 }
 
-fn seek_dupes(i: usize, ids: &[String]) {
-    let mut rem_set = HashSet::new();
-    for id in ids {
-        let dupe = id.clone();
-        let mut removed = format!("{}{}", &dupe[..i], &dupe[i+1..]);
-        if rem_set.contains(&removed) {
-            println!("Duplicate found: {}", removed);
-        } else {
-            rem_set.insert(removed);
+fn seek_dupes(ids: &Vec<String>) -> String {
+    let id_len = ids[0].len();
+    for i in 0..id_len {
+        let mut rem_set = HashSet::new();
+        for id in ids {
+            let dupe = id.clone();
+            let mut removed = format!("{}{}", &dupe[..i], &dupe[i+1..]);
+            if rem_set.contains(&removed) {
+                return String::from(removed);
+            } else {
+                rem_set.insert(removed);
+            }
         }
     }
+    String::from("Failed to find duplicate!")
+}
+
+fn parse_input(input: &String) -> Vec<String> {
+    input.trim().lines()
+         .map(|l| String::from(l))
+         .collect()
 }
 
 fn main() {
-    let f = File::open("input").expect("Failed to open input.");
-    let f = BufReader::new(f);
-    let mut ids = Vec::new();
-    for line in f.lines() {
-        ids.push(line.unwrap());
-    }
+    let mut input = String::new();
+    let mut f = File::open("input").expect("Failed to open input.");
+    f.read_to_string(&mut input).expect("Failed to read input.");
 
-    println!("Hash: {}", hash(ids.as_slice()));
+    let ids = parse_input(&input);
 
-    for i in 0..26 {
-        seek_dupes(i, ids.as_slice());
-    }
+    println!("Hash: {}", hash(&ids));
+    println!("Duplicate: {}", seek_dupes(&ids));
 }
